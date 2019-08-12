84b6178ef8dc0c299193520120196133c2f3e7c1
xuyihao <38197742+ericxuyihao@users.noreply.github.com>
2019-08-12 15:36:21 +0800

    first ediion

--- QueueAndStack/src/MyCircularQueue.java
+++ QueueAndStack/src/MyCircularQueue.java
@@ -3,8 +3,8 @@
  * @date 2019/08/12
  */
 
-public class CircleQueue {
+public class MyCircularQueue {
     public static void main(String[] args) {
-        System.out.println("args = " + args);
+
     }
 }
