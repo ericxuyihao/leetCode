8e55de8d84681aff89a4245ba138be110a4aa350
xuyihao <38197742+ericxuyihao@users.noreply.github.com>
2019-08-14 13:39:19 +0800

    first ediion

---
+++ QueueAndStack/src/BFS.java
@@ -0,0 +1,10 @@
+/**
+ * @author xuyihao
+ * @date 2019/08/14
+ */
+
+public class BFS {
+    public int public int numIslands(char[][] grid) {
+
+    }
+}
--- QueueAndStack/src/MyCircularQueue.java
+++ QueueAndStack/src/MyCircularQueue.java
@@ -17,5 +17,56 @@ public class MyCircularQueue {
         size = k;
     }
 
+    public boolean isFull() {
+        return ((tail + 1) % size) == head;
+    }
+
+    public boolean isEmpty() {
+        return head == -1;
+    }
+
+    /**
+     * Get the last item from the queue.
+     */
+    public int Rear() {
+        if (isEmpty() == true) {
+            return -1;
+        }
+        return data[tail];
+    }
+
+    public int Front() {
+        if ((isEmpty() == true)) {
+            return -1;
+        }
+        return data[head];
+    }
+
+
+    public boolean enQueue(int value) {
+        if (isFull() == true) {
+            return false;
+        }
+        if (isEmpty() == true) {
+            head = 0;
+        }
+        tail = (tail + 1) % size;
+        data[tail] = value;
+        return true;
+    }
+
+    public boolean deQueue() {
+        if (isEmpty() == true) {
+            return false;
+        }
+        if (head == tail) {
+            head = -1;
+            tail = -1;
+            return true;
+        }
+        head = (head + 1) % size;
+        return true;
+    }
+
 
 }
